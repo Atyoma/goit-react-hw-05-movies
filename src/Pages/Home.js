9b30e256582a493e9b1d54a142bbda3ab973a548
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2022-05-05 20:02:42 +0300

    add homePage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,60 @@
+// import { NavLink, Outlet } from 'react-router-dom';
+// import { useState, useEffect } from 'react';
+// import api from '../Service/api-service';
+// import s from './Nav.module.css';
+// // import MoviesList from 'components/MoviesList/MoviesList';
+
+// export default function Home() {
+//   const [movies, setMovies] = useState([]);
+
+//   useEffect(() => {
+//     api
+//       .fetchMovies()
+//       .then(setMovies)
+//       .catch(error => 'error');
+//   }, []);
+//   // const poster = movies.map(movie => movie.poster_path);
+//   // let data = movies.results;
+//   console.log(movies);
+
+//   return (
+//     <>
+//       <header>
+//         <nav>
+//           <NavLink
+//             exact="true"
+//             to="/"
+//             className={s.link}
+//             style={isActive => ({
+//               color: isActive ? '#2196f3' : '#2a363b',
+//             })}
+//           >
+//             Home
+//           </NavLink>
+//           <NavLink
+//             to="/movies"
+//             className={s.link}
+//             style={isActive => ({
+//               color: isActive ? '#2196f3' : '#2a363b',
+//             })}
+//           >
+//             Movies
+//           </NavLink>
+//           <hr />
+//           <h1>Trending today</h1>
+//           <ul>{movies && movies.map(movie => <li>{movie.id}</li>)}</ul>
+//           <Outlet />
+//         </nav>
+//       </header>
+//     </>
+//   );
+// }
+
 import { NavLink, Outlet } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import api from '../Service/api-service';
 import s from './Nav.module.css';
-// import MoviesList from 'components/MoviesList/MoviesList';
+// import MoviesList from 'components/MoviesList/MoviesL ist';
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
@@ -10,39 +62,35 @@ export default function Home() {
   useEffect(() => {
     api
       .fetchMovies()
-      .then(setMovies)
+      .then(({ results }) => {
+        setMovies(results);
+      })
       .catch(error => 'error');
-  }, [movies]);
-  // const poster = movies.map(movie => movie.poster_path);
-  let data = movies.results;
-  console.log(data);
+  }, []);
 
   return (
     <>
       <header>
-        <nav>
+        <nav className={s.nav}>
           <NavLink
             exact="true"
             to="/"
-            className={s.link}
-            style={isActive => ({
-              color: isActive ? '#2196f3' : '#2a363b',
-            })}
+            className={({ isActive }) => (isActive ? s.active : s.notActive)}
           >
             Home
           </NavLink>
           <NavLink
             to="/movies"
-            className={s.link}
-            style={isActive => ({
-              color: isActive ? '#2196f3' : '#2a363b',
-            })}
+            className={({ isActive }) => (isActive ? s.active : s.notActive)}
           >
             Movies
           </NavLink>
           <hr />
           <h1>Trending today</h1>
-          <ul>{movies && movies.map(movie => <li>{movie.id}</li>)}</ul>
+          <ul>
+            {movies &&
+              movies.map(movie => <li key={movie.id}>{movie.title}</li>)}
+          </ul>
           <Outlet />
         </nav>
       </header>
