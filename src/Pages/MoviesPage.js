21321c4f59d299241c2cf95418bc8a8dc314246d
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2022-05-10 21:41:39 +0300

    Update MoviesPage.js

--- src/Pages/MoviesPage.js
+++ src/Pages/MoviesPage.js
@@ -10,7 +10,6 @@ export default function MoviesPage() {
 
   const getQuery = useCallback(e => {
     const inputQuery = e.currentTarget.elements.searchInput.value;
-    // console.log(inputQuery);
     if (inputQuery === '') {
       toast.warning('Please enter search name'); //info    success   warning  error  default
       return;
@@ -23,11 +22,6 @@ export default function MoviesPage() {
       })
       .catch(error => 'error');
   }, []);
-
-  // const handleSubmit = e => {
-  //   e.preventDefault();
-  //   ;
-  // };
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -65,66 +59,3 @@ export default function MoviesPage() {
     </div>
   );
 }
-
-// import { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
-// import * as moviesAPI from '../../services/movies-api';
-// import {
-//   SearchForm,
-//   SearchFormButton,
-//   SearchFormInput,
-// } from './MoviesPage.styled';
-
-// export default function MoviesPage() {
-//   const [query, setQuery] = useState('');
-//   const [searchResult, setSearchResult] = useState(null);
-
-//   useEffect(() => {
-//     if (!query) {
-//       return;
-//     }
-//     moviesAPI.fetchSearchMovie(query).then(({ results }) => {
-//       setSearchResult(results);
-//     });
-//   }, [query]);
-
-//   const handleFormSubmit = evt => {
-//     evt.preventDefault();
-//     setQuery(document.forms.queryForm.query.value);
-//     document.forms.queryForm.query.value = '';
-//   };
-
-//   return (
-//     <>
-//       <SearchForm name="queryForm" onSubmit={handleFormSubmit}>
-//         <SearchFormInput
-//           type="text"
-//           name="query"
-//           autocomplete="off"
-//           autoFocus
-//           placeholder="Search movies "
-//         />
-//         <SearchFormButton type="submit">Search</SearchFormButton>
-//       </SearchForm>
-
-//       {searchResult && (
-//         <>
-//           <h2>Result by search "{query}"</h2>
-//           <ul>
-//             {searchResult.map(el => (
-//               <li key={el.id}>
-//                 <Link
-//                   to={{
-//                     pathname: `./movies/${el.id}`,
-//                   }}
-//                 >
-//                   {el.title}
-//                 </Link>
-//               </li>
-//             ))}
-//           </ul>
-//         </>
-//       )}
-//     </>
-//   );
-// }
