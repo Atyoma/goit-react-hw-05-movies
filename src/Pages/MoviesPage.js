b21dee267fd177960bcdb96f4770816d27cbb566
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2022-05-10 10:04:01 +0300

    fixed

--- src/Pages/MoviesPage.js
+++ src/Pages/MoviesPage.js
@@ -42,17 +42,16 @@ export default function MoviesPage() {
         </button>
       </form>
       <ul>
-        {movies
-          ? movies.map(movie => {
-              return (
-                <li key={movie.id}>
-                  <Link to={`/movies/${movie.id}`}>
-                    {movie.title ?? movie.name}
-                  </Link>
-                </li>
-              );
-            })
-          : null}
+        {movies &&
+          movies.map(movie => {
+            return (
+              <li key={movie.id}>
+                <Link to={`/movies/${movie.id}`}>
+                  {movie.title ?? movie.name}
+                </Link>
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
